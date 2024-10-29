ad544af71f79cccb9441cc81399545ef3369e916
moisessmello <moisesmelo70@gmail.com>
2024-10-28 21:57:23 -0300

    subindo

---
+++ src/app/dataAgendamento/[id]/page.js
@@ -0,0 +1,137 @@
+"use client";
+
+import Pagina from "@/components/Pagina";
+import { useState } from "react";
+import { Button, Col, Container, Row, Modal } from "react-bootstrap";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { format, parse, startOfWeek, getDay } from "date-fns";
+import { Image,} from "react-bootstrap";
+import ptBR from "date-fns/locale/pt-BR";
+
+const locales = { "pt-BR": ptBR };
+const localizer = dateFnsLocalizer({ format, parse, startOfWeek, getDay, locales });
+
+export default function Page({params}) {
+  const [dataSelecionada, setDataSelecionada] = useState(null);
+  const [horarioSelecionado, setHorarioSelecionado] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  //Buscando dados de agendamento
+  const agendamentos = JSON.parse(localStorage.getItem("vaptvupt"));
+  const agendamentoBuscado = agendamentos.find(item => item.id == params.id);
+
+  // Eventos de agendamento
+  const [eventos, setEventos] = useState([]);
+  
+  // Abre o modal de agendamento
+  const handleSlotSelect = ({ start }) => {
+    setDataSelecionada(start);
+    setShowModal(true);
+  };
+
+  function salvarAgendamento() {
+    if (dataSelecionada && horarioSelecionado) {
+      const novoEvento = {
+        title: `Agendado para ${horarioSelecionado}`,
+        start: dataSelecionada,
+        end: dataSelecionada,
+      };
+      setEventos([...eventos, novoEvento]);
+      localStorage.setItem("vaptvupt", JSON.stringify([...eventos, novoEvento]));
+      setShowModal(false);
+      alert("Agendamento salvo com sucesso!");
+    } else {
+      alert("Por favor, selecione uma data e horário.");
+    }
+  } 
+
+  return (
+    <>
+       <Pagina titulo="Agendar Data e Hora">
+        <Container>
+          <Row className="justify-content-center mt-5">
+            <Col md={12}>
+              <Calendar
+                localizer={localizer}
+                events={eventos}
+                startAccessor="start"
+                endAccessor="end"
+                selectable
+                culture="pt-BR" // Configura o calendário para o português do Brasil
+                style={{ height: 500, margin: "50px" }}
+                onSelectSlot={handleSlotSelect}
+              />
+            </Col>
+          </Row>
+        </Container>
+
+        {/* Modal para Seleção de Horário */}
+        <Modal show={showModal} onHide={() => setShowModal(false)}>
+          <Modal.Header closeButton>
+            <Modal.Title>Selecione o Horário</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <select
+              className="form-control"
+              value={horarioSelecionado}
+              onChange={(e) => setHorarioSelecionado(e.target.value)}
+            >
+              <option value="">Selecione o horário</option>
+              <option value="08:00">08:00</option>
+              <option value="09:00">09:00</option>
+              <option value="10:00">10:00</option>
+              <option value="11:00">11:00</option>
+              <option value="14:00">14:00</option>
+              <option value="15:00">15:00</option>
+              <option value="16:00">16:00</option>
+            </select>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShowModal(false)}>
+              Cancelar
+            </Button>
+            <Button variant="primary" onClick={salvarAgendamento}>
+              Confirmar Agendamento
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Pagina>
+
+      <footer className="bg-dark text-white text-center p-4 mt-5">
+        <Container>
+          <Row>
+            <Col md={4}>
+              <Image
+                src="https://vaptvupt.go.gov.br/assets/externo/img/logo-vapt-vupt.png"
+                alt="Logo VaptVupt"
+                style={{ width: "100px" }}
+              />
+            </Col>
+            <Col md={4}>
+              <h5>Localização</h5>
+              <p>
+                Avenida Central, 1234
+                <br />
+                Centro, Águas Lindas - GO - 72.910-000
+              </p>
+            </Col>
+            <Col md={4}>
+              <h5>Contato</h5>
+              <p>
+                Telefone: (61) 3613-0000
+                <br />
+                Email: contato@vaptvupt.com
+              </p>
+            </Col>
+          </Row>
+          <Row className="mt-4">
+            <Col md={12}>
+              <p>&copy; 2024 VaptVupt - Todos os direitos reservados</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
+  );
+}
--- src/app/vaptvupt/form/[[...id]]/page.js
+++ src/app/vaptvupt/form/[[...id]]/page.js
@@ -1,10 +1,11 @@
 "use client";
 
 import Pagina from "@/components/Pagina";
+import apiLocalidade from "@/services/apiLocalidade";
 import { Formik } from "formik";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   Card,
@@ -18,17 +19,36 @@ import {
 import { FaCheck } from "react-icons/fa";
 import { MdOutlineArrowBack } from "react-icons/md";
 import { MdOutlineDateRange } from "react-icons/md";
+import { v4 } from "uuid";
 
 export default function Page() {
   const route = useRouter();
   const [isClicked, setIsClicked] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
+  const [paises, setPaises] = useState([])
+  const [ufs, setUfs] = useState([])
+  const [cidades, setCidades] = useState([])
+  const [camposBrasil, setCamposBrasil] = useState(false)
+
+  useEffect(() => {
+
+    apiLocalidade.get(`paises`).then(resultado => {
+      setPaises(resultado.data)
+    })
+
+    apiLocalidade.get(`estados?orderBy=nome`).then(resultado => {
+      setUfs(resultado.data)
+    })
+
+  }, [])
+
   function salvar(dados) {
     const vaptvupt = JSON.parse(localStorage.getItem("vaptvupt")) || [];
+    dados.id = v4()
     vaptvupt.push(dados);
     localStorage.setItem("vaptvupt", JSON.stringify(vaptvupt)); //setItem é para inserir
-    return route.push("/vaptvupt");
+    return route.push(`/dataAgendamento/${dados.id}`);
   }
 
   const handleCardClick = () => {
@@ -54,68 +74,104 @@ export default function Page() {
                   servico: "",
                   uf: "",
                   municipio: "",
-                  pais: "",
+                  pais: "Brasil",
                 }}
                 onSubmit={(values) => salvar(values)}
               >
-                {({ values, handleChange, handleSubmit }) => (
-                  <Form className="w-100">
-                    <Form.Group className="mb-3" controlId="nome">
-                      <Form.Label>Órgão (opcional)</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="orgao"
-                        value={values.orgao}
-                        onChange={handleChange("orgao")}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="servico">
-                      <Form.Label>Serviços</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="servico"
-                        value={values.servico}
-                        onChange={handleChange("servico")}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="uf">
-                      <Form.Label>UF</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="uf"
-                        value={values.uf}
-                        onChange={handleChange("uf")}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="municipio">
-                      <Form.Label>Cidade</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="municipio"
-                        value={values.municipio}
-                        onChange={handleChange("municipio")}
-                      />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="pais">
-                      <Form.Label>País</Form.Label>
-                      <Form.Control
-                        type="text"
-                        name="pais"
-                        value={values.pais}
-                        onChange={handleChange("pais")}
-                      />
-                    </Form.Group>
+                {({ values,
+                  handleChange,
+                  handleSubmit }) => {
 
-                    <div className="text-center mt-4">
-                      <Link href="/vaptvupt" className="btn btn-danger me-3">
-                        <MdOutlineArrowBack /> Voltar
-                      </Link>
-                      <Button onClick={handleSubmit} variant="success">
-                        Salvar <FaCheck />
-                      </Button>
-                    </div>
-                  </Form>
-                )}
+                  useEffect(() => {
+                    setCamposBrasil(values.pais == 'Brasil')
+                  }, [values.pais])
+
+                  useEffect(() => {
+                    apiLocalidade.get(`estados/${values.uf}/municipios`).then(resultado => {
+                      setCidades(resultado.data)
+                    })
+                  }, [values.uf])
+
+                  return (
+                    <Form className="w-100">
+                      <Form.Group className="mb-3" controlId="nome">
+                        <Form.Label>Órgão (opcional)</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="orgao"
+                          value={values.orgao}
+                          onChange={handleChange("orgao")}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="servico">
+                        <Form.Label>Serviços</Form.Label>
+                        <Form.Control
+                          type="text"
+                          name="servico"
+                          value={values.servico}
+                          onChange={handleChange("servico")}
+                        />
+                      </Form.Group>
+                      <Form.Group className="mb-3" controlId="pais">
+                        <Form.Label>País</Form.Label>
+                        <Form.Select
+                          name="pais"
+                          value={values.pais}
+                          onChange={handleChange('pais')}
+                        >
+                          <option value=''>Selecione</option>
+                          {paises.map(item => (
+                            <option key={item.nome} value={item.nome}>
+                              {item.nome}
+                            </option>
+                          ))}
+                        </Form.Select>
+                      </Form.Group>
+                      {camposBrasil &&
+                        <>
+                          <Form.Group className="mb-3" controlId="uf">
+                            <Form.Label>UF</Form.Label>
+                            <Form.Select
+                              name="uf"
+                              value={values.uf}
+                              onChange={handleChange('uf')}
+                            >
+                              <option value=''>Selecione</option>
+                              {ufs.map(item => (
+                                <option key={item.sigla} value={item.sigla}>
+                                  {item.sigla} - {item.nome}
+                                </option>
+                              ))}
+                            </Form.Select>
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="cidade">
+                            <Form.Label>Cidade</Form.Label>
+                            <Form.Select
+                              name="cidade"
+                              value={values.cidade}
+                              onChange={handleChange('cidade')}
+                            >
+                              <option value=''>Selecione</option>
+                              {cidades.map(item => (
+                                <option key={item.nome} value={item.nome}>
+                                  {item.nome}
+                                </option>
+                              ))}
+                            </Form.Select>
+                          </Form.Group>
+                        </>
+                      }
+                      <div className="text-center mt-4">
+                        <Link href="/vaptvupt" className="btn btn-danger me-3">
+                          <MdOutlineArrowBack /> Voltar
+                        </Link>
+                        <Button onClick={handleSubmit} variant="success">
+                          Salvar <FaCheck />
+                        </Button>
+                      </div>
+                    </Form>
+                  )
+                }}
               </Formik>
             </Col>
 
@@ -149,13 +205,13 @@ export default function Page() {
         {/* Modal de erro */}
         <Modal show={showModal} onHide={handleCloseModal} centered>
           <Modal.Header >
-          <Modal.Title className="w-100 text-center">
-                <h1>Atenção</h1>
-              </Modal.Title>
+            <Modal.Title className="w-100 text-center">
+              <h1>Atenção</h1>
+            </Modal.Title>
           </Modal.Header>
           <Modal.Body className="text-center">
-              Primeiro escolha Serviço e Município.
-            </Modal.Body>
+            Primeiro escolha Serviço e Município.
+          </Modal.Body>
           <Modal.Footer className="d-flex justify-content-center">
             <Button variant="success" onClick={handleCloseModal}>
               ok
